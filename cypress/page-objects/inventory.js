600812bea0d76bab4305cd2e527c2f9b30da085e
Colleen <colleen.williams1905@gmail.com>
2023-03-16 20:02:18 -0500

    inventoryTestCases

---
+++ cypress/page-objects/inventory.js
@@ -0,0 +1,42 @@
+class productsInventory {
+
+    get sortSelectorButton() {
+        return ('.product_sort_container')
+    }
+    get cartItemName() {
+        return ('.inventory_item_name')
+    }
+    get itemPrice() {
+        return ('.inventory_item_price')
+    }
+    get addCartButton() {
+        return ('#add-to-cart-sauce-labs-bolt-t-shirt')
+    }
+    get cartItem() {
+        return ('#item_1_title_link')
+    }
+    get removeSauceLabBoltButton() {
+        return ('#remove-sauce-labs-bolt-t-shirt')
+    }
+    get cartIcon() {
+        return ('.shopping_cart_link')
+    }
+
+
+    sortProducts(sort) {
+        cy.get(this.sortSelectorButton).select(sort)
+    }
+    addToCartFromProductPage() {
+        cy.get(this.cartItem).click()
+        cy.get(this.addCartButton).click()
+    }
+    removeItemFromProductPage() {
+        cy.get(this.addCartButton).click()
+        cy.get(this.removeSauceLabBoltButton).click()
+    }
+    navigateToCart() {
+        cy.get(this.cartIcon).click()
+    }
+
+}
+export default new Products()
---
+++ cypress/test-files/inventory.cy.js
@@ -0,0 +1,53 @@
+import loginPage from "../page-objects/authentication";
+import productsInventory from "../page-objects/inventory";
+import ProductsData from "../data-files/inventory-data";
+
+
+
+describe('Inventory Page', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('should sort products from A-Z', () => {
+        loginPage.login('standard_user', 'secret_sauce')
+        ProductsData.products.sort()
+        cy.get(productsInventory.cartItemName).each(($elem, index) => {
+            expect($elem.text()).equal(ProductsData.products[index].name)
+        })
+
+    })
+    it('should sort product prices from low to high', () => {
+        loginPage.login('standard_user', 'secret_sauce')
+        productsInventory.sortProducts(ProductsData.sort['Low to High'])
+        ProductsData.products.sort((a, b) => a.price - b.price)
+        cy.get(productsInventory.itemPrice).each(($elem, index) => {
+            expect($elem.text()).equal(`$${ProductsData.products[index].price}`)
+        })
+
+    })
+
+    it('should add Sauce Labs Bolt T-shirt from the product page', () => {
+        loginPage.login('standard_user', 'secret_sauce')
+        cy.get(loginPage.inventoryList).should('be.visible')
+
+        productsInventory.addToCartFromProductPage()
+        cy.get(productsInventory.removeSauceLabBoltButton).should('be.visible')
+        productsInventory.navigateToCart()
+        cy.get(productsInventory.cartItemName).should('have.text', 'Sauce Labs Bolt T-Shirt')
+
+    })
+
+    it('should remove Sauce Labs Bolt T-shirt from the product page', () => {
+        loginPage.login('standard_user', 'secret_sauce')
+        cy.get(loginPage.inventoryList).should('be.visible')
+
+        productsInventory.removeItemFromProductPage()
+        cy.get(productsInventory.addCartButton).should('be.visible')
+
+        productsInventory.navigateToCart()
+        cy.get(productsInventory.cartItemName).should('not.exist')
+
+    })
+
+})
